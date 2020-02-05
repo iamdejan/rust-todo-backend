c447b506697eaee10c309a8fd5be0f5cd0157bea
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2020-02-05 19:14:56 +0700

    Update routes.rs

--- src/application/routes.rs
+++ src/application/routes.rs
@@ -11,20 +11,6 @@ pub async fn route_initial_test(request: HttpRequest) -> HttpResponse {
 #[allow(non_snake_case)]
 pub async fn get_all_TODOs(memo_handler: web::Data<MemoHandler<'_>>) -> HttpResponse {
     let response_data: Vec<Memo> = memo_handler.get_all();
-
-    // for result in all_data_cursor {
-    //     if result.is_ok() {
-    //         let data: bson::ordered::OrderedDocument = result.unwrap();
-    //         let memo_parse_result = bson::from_bson(bson::Bson::Document(data));
-    //         if memo_parse_result.is_ok() != true {
-    //             return HttpResponse::InternalServerError().body("");
-    //         }
-    //         response_data.push(memo_parse_result.unwrap());
-    //     } else {
-    //         return HttpResponse::InternalServerError().body("");
-    //     }
-    // }
-
     return HttpResponse::Ok().json(response_data);
 }
 
